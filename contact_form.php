17c1f32e22c23323d067f6c43768bf704e1698a7
Adedoyin Yinka <timmytune002@gmail.com>
2018-07-07 15:19:22 +0100

    dddddd

--- contact_form.php
+++ contact_form.php
@@ -14,21 +14,7 @@ if(isset($_POST['email'])) {
  
      
  
-    function died($error) {
- 
-        // your error code can go here
- 
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
- 
-        echo "These errors appear below.<br /><br />";
- 
-        echo $error."<br /><br />";
- 
-        echo "Please go back and fix these errors.<br /><br />";
- 
-        die();
- 
-    }
+    
  
      
  
